37d8a0708ab2df03df6ab95260cb06c096c8bbf6
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2020-11-05 19:58:49 +0000

    :recycle: Refatorando Promises para Async/Await

--- modulo-01/index.js
+++ modulo-01/index.js
@@ -41,35 +41,73 @@ function obterEndereco(idUsuario, callback) {
    }, 2000);
 }
 
-obterUsuario()
-   .then(function(usuario) {
-      return obterTelefone(usuario.id)
-         .then(function(telefone) {
-            return {
-               usuario: {
-                  id: usuario.id,
-                  nome: usuario.nome,
-                  dataNascimento: usuario.dataNascimento,
-               },
-               telefone: telefone
-            }
-         })
-   })
-   .then(function(resultado) {
-      const endereco = obterEnderecoAsync(resultado.usuario.id);
-      return endereco.then(function(endereco) {
-         return { ...resultado, endereco }
-      })
-   })
-   
-   .then(function(resultado) {
-      console.log('[resultado]:', resultado);   
-   })
-   
-   .catch(function(error) {
-      console.log('DEU RUIM NA PROMISE', error);
-   })
 
+// UTILIZANDO O CONCEITO DE ASYNC/AWAIT 
+async function main() {
+   
+   try {
+      console.time('[medida-promise]');
+      
+      const usuario = await obterUsuario();
+      // const telefone = await obterTelefone(usuario.id);
+      // const endereco = await obterEnderecoAsync(usuario.id);
+      const resultado = await Promise.all([
+         obterTelefone(usuario.id),
+         obterEnderecoAsync(usuario.id),
+      ]);
+
+      const telefone = resultado[0];
+      const endereco = resultado[1];
+
+      console.log(`
+         Nome: ${usuario.nome}
+         Endereco: ${endereco.rua}, ${endereco.numero}
+         Telefone: (${telefone.ddd})${telefone.telefone}
+      `)      
+
+      console.timeEnd('[medida-promise]');
+
+   } catch (error) {
+      console.error('DEU RUIM', error);
+   }
+   
+}
+
+main();
+
+// ================ UTILIZANDO O CONCEITO DE PROMISES ===================
+// obterUsuario()
+//    .then(function(usuario) {
+//       return obterTelefone(usuario.id)
+//          .then(function(telefone) {
+//             return {
+//                usuario: {
+//                   id: usuario.id,
+//                   nome: usuario.nome,
+//                   dataNascimento: usuario.dataNascimento,
+//                },
+//                telefone: telefone
+//             }
+//          })
+//    })
+//    .then(function(resultado) {
+//       const endereco = obterEnderecoAsync(resultado.usuario.id);
+//       return endereco.then(function(endereco) {
+//          return { ...resultado, endereco }
+//       })
+//    })
+   
+//    .then(function(resultado) {
+//       console.log('[resultado]:', resultado);   
+//    })
+   
+//    .catch(function(error) {
+//       console.log('DEU RUIM NA PROMISE', error);
+//    })
+// =====================================================================
+
+
+// ================ UTILIZANDO O CONCEITO DE CALLBACKS =================
 
 // obterUsuario(function resolverUsuario(erroUsuario, usuario) {
 //    if(erroUsuario) {
