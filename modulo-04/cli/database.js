b4195fdb6818e9e4bddd67a80d547965cce1d20a
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2021-02-04 15:56:21 +0000

    :sparkles: Adicionando método de update

--- modulo-04/cli/database.js
+++ modulo-04/cli/database.js
@@ -8,45 +8,87 @@ const writeFileAsync = promisify(writeFile);
 // const database = require('./heroes.json);
 
 class Database {
-    constructor() {
-        this.FILE_NAME = 'heroes.json';
-    }
+   constructor() {
+      this.FILE_NAME = 'heroes.json';
+   }
 
-    async obterDadosArquivos() {
-        const file = await readFileAsync(this.FILE_NAME, 'utf8');
-        return JSON.parse(file.toString());        
-    };
+   async obterDadosArquivos() {
+      const file = await readFileAsync(this.FILE_NAME, 'utf8');
+      return JSON.parse(file.toString());
+   };
 
-    async escreverArquivos(dados) {
-        await writeFileAsync(this.FILE_NAME, JSON.stringify(dados));
-        return true;
-    };
+   async escreverArquivos(dados) {
+      await writeFileAsync(this.FILE_NAME, JSON.stringify(dados));
+      return true;
+   };
 
-    async listar(id) {
-        const data = await this.obterDadosArquivos();
-        const heroFiltered = data.filter(hero => (id ? (hero.id === id) : true));
-        return heroFiltered;
-    };
+   async listar(id) {
+      const data = await this.obterDadosArquivos();
+      const heroFiltered = data.filter(hero => (id ? (hero.id === id) : true));
+      return heroFiltered;
+   };
 
-    async cadastrar(hero) {
-        // Objetivo: obter o arquivo modificar os dados e reescrever o arquivo
-        const data = await this.obterDadosArquivos();
-        const id = hero.id <= 2 ? hero.id : Date.now();
+   async cadastrar(hero) {
+      // Objetivo: obter o arquivo modificar os dados e reescrever o arquivo
+      const data = await this.obterDadosArquivos();
+      const id = hero.id <= 2 ? hero.id : Date.now();
 
-        const heroWithId = {
-            id,
-            ...hero
-        };
+      const heroWithId = {
+         id,
+         ...hero
+      };
 
-        const newData = [
-            ...data,
-            heroWithId
-        ];
-        
-        const response = await this.escreverArquivos(newData);
+      const newData = [
+         ...data,
+         heroWithId
+      ];
 
-        return response;
-    }
+      const response = await this.escreverArquivos(newData);
+
+      return response;
+   }
+
+   async remover(id) {
+      
+      if(!id) {
+         return await this.escreverArquivos([])
+      }
+
+      const dados = await this.obterDadosArquivos();
+      const indice = dados.findIndex(item => item.id === parseInt(id));
+
+      if(indice === -1) {
+         throw Error('O usuário informado não existe!');
+      }
+
+      dados.splice(indice, 1);
+
+      return await this.escreverArquivos(dados);
+
+   }
+
+   async atualizar(id, modificacoes) {
+      
+      const dados = await this.obterDadosArquivos();
+      const indice = dados.findIndex(item => item.id === parseInt(id));
+
+      if(indice === -1) {
+         throw Error('O herói informado não existe');
+      }
+
+      const atual = dados[indice];
+      const objetoAtualizado = {
+         ...atual,
+         ...modificacoes
+      };      
+               
+      dados.splice(indice, 1);
+
+      return await this.escreverArquivos([
+         ...dados,
+         objetoAtualizado
+      ]);      
+   };
 
 }
 
--- modulo-04/cli/test.spec.js
+++ modulo-04/cli/test.spec.js
@@ -7,24 +7,55 @@ const DEFAULT_HERO = {
     power: 'Speed'
 };
 
+const DEFAULT_HERO_UPDATE = {
+   id: 2,
+   name: 'Lanterna Verde',
+   power: 'Energia do Anel'
+}
+
 describe('Suite de manipulação de Herois', () => {
-    before(async () => {
-        await database.cadastrar(DEFAULT_HERO);
-    });
+   before(async () => {
+      await database.cadastrar(DEFAULT_HERO);
+      await database.cadastrar(DEFAULT_HERO_UPDATE);
+   });
+   
+   it('Deve pesquisa um herói', async () => {
+      const expected = DEFAULT_HERO;       
+      const [response] = await database.listar(expected.id);
+      assert.deepEqual(response, expected);
+   });   
+   
+   it('Deve cadastrar um herói', async () => {
+      const expected = DEFAULT_HERO;
+      const response = await database.cadastrar(DEFAULT_HERO);
+      const [actual] = await database.listar(DEFAULT_HERO.id);
+      assert.deepEqual(actual, expected);
+   });
 
+   it('Deve remover um herói por id', async () => {
+      const expected = true;      
 
-    it('Deve pesquisa um herói', async () => {
-        
-        const expected = DEFAULT_HERO;       
-        const [response] = await database.listar(expected.id);
-        assert.deepEqual(response, expected);
-    });
-    
-    
-    it('Deve cadastrar um herói', async () => {
-        const expected = DEFAULT_HERO;
-        const response = await database.cadastrar(DEFAULT_HERO);
-        const [actual] = await database.listar(DEFAULT_HERO.id);
-        assert.deepEqual(actual, expected);
-    });
+      const response = await database.remover(DEFAULT_HERO.id);      
+
+      assert.deepEqual(response, expected);
+   });
+
+   it('Deve atualizar um herói pelo id', async () => {
+      const expected = {
+         ...DEFAULT_HERO_UPDATE,
+         name: 'Batman',
+         power: 'Dinheiro'
+      };
+
+      const novoDado = {
+         name: 'Batman',
+         power: 'Dinheiro'
+      }
+
+      await database.atualizar(DEFAULT_HERO_UPDATE.id, novoDado);
+      const [response] = await database.listar(DEFAULT_HERO_UPDATE.id);
+
+      assert.deepEqual(response, expected);
+   });
+
 });
